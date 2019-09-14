b9c0b330d0867125edd124acb89ff1a59cc679ab
PyvesB <PyvesDev@gmail.com>
2019-09-14 19:21:26 +0100

    Stopped using preserved rbx register

--- ArraySum.asm
+++ ArraySum.asm
@@ -21,16 +21,16 @@ Java_JNIArraySum_computeNativeArraySum:
     pop rcx                   ; retrieve array length
     lea rcx, [rax + 4 * rcx]  ; compute loop end address (after last array element)
     mov rdx, rax              ; set elems parameter for future ReleaseIntArrayElements
-    xor ebx, ebx              ; initialise sum accumulator
+    xor r8d, r8d              ; initialise sum accumulator
     add_element:
         movsxd r9, dword [rax]; get current element
-        add rbx, r9           ; add to sum
+        add r8, r9            ; add to sum
         add rax, 4            ; move array pointer to next element
         cmp rax, rcx          ; has all array been processed?
         jne add_element
     pop rdi                   ; retrieve JNIEnv
     pop rsi                   ; retrieve Java array pointer
-    push rbx                  ; store sum result
+    push r8                   ; store sum result
     mov rax, [rdi]            ; get location of JNI function table
     call [rax + ReleaseIntArrayElements]
     pop rax                   ; retrieve sum result
